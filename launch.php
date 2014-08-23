00265cb5bb72e51e0c3096344ad888ae7e6c4754
Matt Beall <mbeall@starverte.com>
2014-08-23 15:49:38 -0600

    Create launch.php

---
+++ launch.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Leftovers (lifepointe theme)
+ * Description: Miscellaneous items leftover from old LifePointe theme
+ */
+ 
+ include_once dirname( __FILE__ ) . '/admin_style.php';
