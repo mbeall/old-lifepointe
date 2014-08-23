45a1c6bcfbe4dc3520d8d3c15f518efd73205823
Matt Beall <mbeall@starverte.com>
2014-08-23 16:03:54 -0600

    Update launch.php

--- launch.php
+++ launch.php
@@ -4,4 +4,13 @@
  * Description: Miscellaneous items leftover from old LifePointe theme
  */
  
- include_once dirname( __FILE__ ) . '/admin_style.php';
+include_once dirname( __FILE__ ) . '/admin-style.php';
+include_once dirname( __FILE__ ) . '/custom-image-editor.php';
+include_once dirname( __FILE__ ) . '/lbpmodal.php';
+include_once dirname( __FILE__ ) . '/media-meta.php';
+include_once dirname( __FILE__ ) . '/misc.php';
+include_once dirname( __FILE__ ) . '/more.php';
+include_once dirname( __FILE__ ) . '/ns.php';
+include_once dirname( __FILE__ ) . '/podcast-tax.php';
+include_once dirname( __FILE__ ) . '/sermon-manager.php';
+include_once dirname( __FILE__ ) . '/shortcodes.php';
